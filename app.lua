9e8d8bd5bfd9ffb7f2065dc72f4fbb9aa15c3ce4
Hector Vido <hector_vido@yahoo.com.br>
2019-11-21 03:59:10 -0300

    Capturado dados do twitter e salvo no banco

--- app.lua
+++ app.lua
@@ -5,24 +5,43 @@ local ltn12 = require("ltn12")
 local https = require("ssl.https")
 local driver = require('luasql.firebird')
 local inspect = require('inspect')
+local date = require('date')
 
 twitter_key = assert (os.getenv('TW_KEY'))
 twitter_secret = assert (os.getenv('TW_SECRET'))
+
+env = assert (driver.firebird())
+firebird = assert (env:connect('localhost:/var/lib/firebird/3.0/data/luafirebird.fdb', 'app', 'zjgNmeaoENepyDaeq2*vs)x)kbNm8L2J'))
 
 local b, c, h = https.request(string.format('http://%s:%s@api.twitter.com/oauth2/token', twitter_key, twitter_secret), 'grant_type=client_credentials')
 local token = json.decode(b)
 
 local res = {}
 local b, c, h = https.request {
-    url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23devops&count=100',
+    url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23devops&count=100&tweet_mode=extended',
     sink = ltn12.sink.table(res),
     headers = {
         authorization = string.format('Bearer %s', token.access_token)
     }
 }
+
 local data = json.decode(table.concat(res))
-print(inspect(data.statuses[1].entities))
-print(table.getn(data.statuses))
+for i, tweet in ipairs(data.statuses) do
+	local user = tweet.user
+	local status, msg = pcall(function()
+			res = assert (firebird:execute(string.format([[
+				INSERT INTO users (id, name, screen_name, location) VALUES (%u, '%s', '%s', '%s')]], user.id, firebird:escape(user.name), firebird:escape(user.screen_name), firebird:escape(user.location))
+			))
+			firebird:commit()
+	end)
+	local created_at = date(tweet.created_at)
+	status, msg = pcall(function()
+	res = assert (firebird:execute(string.format([[
+		INSERT INTO tweets (id, created_at, user_id, text) VALUES (%u, '%s', %u, '%s')]], tweet.id, created_at:fmt('%Y-%m-%d %H:%M:%S'), user.id, firebird:escape(tweet.full_text))
+	))
+	end)
+	firebird:commit()
+end
 
 os.exit()
 
--- vagrant/files/provision.sh
+++ vagrant/files/provision.sh
@@ -21,12 +21,24 @@ ISC_PASSWORD="$SYSDBA_PASSWORD"
 EOF
 
 apt-get update
-apt-get install -y vim git lua5.3 luarocks firebird-dev liblua5.3-dev firebird3.0-server
+apt-get install -y vim git lua5.1 luarocks lua-inspect liblua5.1-dev firebird-dev firebird3.0-server
 apt-get clean
 luarocks install luasql-firebird
 luarocks install lua-cjson 2.1.0-1
+luarocks install date
+
+systemctl stop firebird3.0
 
 echo "CREATE USER app PASSWORD '$APP_PASSWORD';" | isql-fb -u sysdba -p "$SYSDBA_PASSWORD" /var/lib/firebird/3.0/system/security3.fdb
 echo "CREATE DATABASE '/var/lib/firebird/3.0/data/luafirebird.fdb';" | isql-fb -u app -p "$APP_PASSWORD" /var/lib/firebird/3.0/system/security3.fdb
 chown firebird: /var/lib/firebird/3.0/data/luafirebird.fdb
-echo "CREATE TABLE twitter (hashtag VARCHAR(50) NOT NULL PRIMARY KEY, content VARCHAR(100));" | isql-fb -u app -p "$APP_PASSWORD" /var/lib/firebird/3.0/data/luafirebird.fdb
+
+systemctl start firebird3.0
+
+echo "CREATE TABLE tweets (id BIGINT NOT NULL PRIMARY KEY, created_at TIMESTAMP NOT NULL, user_id BIGINT NOT NULL, text VARCHAR(500) NOT NULL);" | isql-fb -u app -p "$APP_PASSWORD" localhost:/var/lib/firebird/3.0/data/luafirebird.fdb
+echo "CREATE TABLE users (id BIGINT NOT NULL PRIMARY KEY, name VARCHAR(50) NOT NULL, screen_name VARCHAR(50) NOT NULL, location VARCHAR(50) NOT NULL);" | isql-fb -u app -p "$APP_PASSWORD" localhost:/var/lib/firebird/3.0/data/luafirebird.fdb
+echo "CREATE TABLE hashtags (id VARCHAR(20) NOT NULL PRIMARY KEY);" | isql-fb -u app -p "$APP_PASSWORD" localhost:/var/lib/firebird/3.0/data/luafirebird.fdb
+echo "CREATE TABLE tweets_hashtags (tweet_id BIGINT NOT NULL, hashtag_id VARCHAR(20) NOT NULL);" | isql-fb -u app -p "$APP_PASSWORD" localhost:/var/lib/firebird/3.0/data/luafirebird.fdb
+echo "ALTER TABLE tweets ADD CONSTRAINT fk_tweet_user FOREIGN KEY (user_id) REFERENCES users(id);" | isql-fb -u app -p "$APP_PASSWORD" localhost:/var/lib/firebird/3.0/data/luafirebird.fdb
+echo "ALTER TABLE tweets_hashtags ADD CONSTRAINT fk_th_tweet FOREIGN KEY (tweet_id) REFERENCES tweets(id);" | isql-fb -u app -p "$APP_PASSWORD" localhost:/var/lib/firebird/3.0/data/luafirebird.fdb
+echo "ALTER TABLE tweets_hashtags ADD CONSTRAINT fk_th_hashtag FOREIGN KEY (hashtag_id) REFERENCES hashtags(id);" | isql-fb -u app -p "$APP_PASSWORD" localhost:/var/lib/firebird/3.0/data/luafirebird.fdb

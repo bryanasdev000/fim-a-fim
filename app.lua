e4e5bae6c5fb616f713b36df5d6ab5ee4cf45029
Hector Vido <hector_vido@yahoo.com.br>
2019-11-21 04:02:51 -0300

    Configurado "autocommit"

--- app.lua
+++ app.lua
@@ -12,6 +12,7 @@ twitter_secret = assert (os.getenv('TW_SECRET'))
 
 env = assert (driver.firebird())
 firebird = assert (env:connect('localhost:/var/lib/firebird/3.0/data/luafirebird.fdb', 'app', 'zjgNmeaoENepyDaeq2*vs)x)kbNm8L2J'))
+firebird:setautocommit(true)
 
 local b, c, h = https.request(string.format('http://%s:%s@api.twitter.com/oauth2/token', twitter_key, twitter_secret), 'grant_type=client_credentials')
 local token = json.decode(b)
@@ -26,21 +27,20 @@ local b, c, h = https.request {
 }
 
 local data = json.decode(table.concat(res))
+
 for i, tweet in ipairs(data.statuses) do
 	local user = tweet.user
 	local status, msg = pcall(function()
-			res = assert (firebird:execute(string.format([[
-				INSERT INTO users (id, name, screen_name, location) VALUES (%u, '%s', '%s', '%s')]], user.id, firebird:escape(user.name), firebird:escape(user.screen_name), firebird:escape(user.location))
-			))
-			firebird:commit()
+		res = assert (firebird:execute(string.format([[
+			INSERT INTO users (id, name, screen_name, location) VALUES (%u, '%s', '%s', '%s')]], user.id, firebird:escape(user.name), firebird:escape(user.screen_name), firebird:escape(user.location))
+		))
 	end)
 	local created_at = date(tweet.created_at)
 	status, msg = pcall(function()
-	res = assert (firebird:execute(string.format([[
-		INSERT INTO tweets (id, created_at, user_id, text) VALUES (%u, '%s', %u, '%s')]], tweet.id, created_at:fmt('%Y-%m-%d %H:%M:%S'), user.id, firebird:escape(tweet.full_text))
-	))
+		res = assert (firebird:execute(string.format([[
+			INSERT INTO tweets (id, created_at, user_id, text) VALUES (%u, '%s', %u, '%s')]], tweet.id, created_at:fmt('%Y-%m-%d %H:%M:%S'), user.id, firebird:escape(tweet.full_text))
+		))
 	end)
-	firebird:commit()
 end
 
 os.exit()

f2f960a97d5980024959a91fd3214b1fc9d4be78
Hector Vido <hector_vido@yahoo.com.br>
2019-11-25 03:39:18 -0300

    Atualizado o migration

--- migrations/app.sql
+++ migrations/app.sql
@@ -6,7 +6,7 @@ CREATE USER app PASSWORD 'zjgNmeaoENepyDaeq2*vs)x)kbNm8L2J';
 -- as APP
 CREATE DATABASE '/var/lib/firebird/3.0/data/luafirebird.fdb';
 CREATE TABLE tweets (id BIGINT NOT NULL PRIMARY KEY, created_at TIMESTAMP NOT NULL, user_id BIGINT NOT NULL, text VARCHAR(500) NOT NULL);
-CREATE TABLE users (id BIGINT NOT NULL PRIMARY KEY, name VARCHAR(50) NOT NULL, followers_count INT NOT NULL, screen_name VARCHAR(50) NOT NULL, location VARCHAR(50) NOT NULL);
+CREATE TABLE users (id BIGINT NOT NULL PRIMARY KEY, name VARCHAR(100) NOT NULL, followers_count INT NOT NULL, screen_name VARCHAR(50) NOT NULL, location VARCHAR(100) NOT NULL);
 CREATE TABLE hashtags (id VARCHAR(20) NOT NULL PRIMARY KEY);
 CREATE TABLE tweets_hashtags (tweet_id BIGINT NOT NULL, hashtag_id VARCHAR(20) NOT NULL);
 
